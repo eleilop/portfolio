6c1222fe561e961e2fb29ce5b70584b5ebbe3a20
eleilop <eloyleivalopez48@gmail.com>
2023-11-23 23:29:23 +0100

    Halloween Party | Pumpkin, moon, grave, bat animations

---
+++ HALLOWEEN/js/functions.js
@@ -0,0 +1,19 @@
+/***********************************/
+/*    JavaScript para Halloween    */
+/***********************************/
+
+function show(element) {
+    document.getElementById(element).style.visibility="visible"; /* Hace visible el elemento */
+}   
+
+function hide(element) {
+    document.getElementById(element).style.visibility="hidden"; /* Esconde el elemento */
+}
+
+function sound(element) {
+    document.getElementById(element).play(); /* Reproduce el sonido */
+}
+
+function silence(element) {
+    document.getElementById(element).pause(); /* Pausa el sonido */
+}
